0c52f4a4895e0b8962535ffce632a33d4afe1a6f
subiksha-venkidusamy <124667477+subiksha-venkidusamy@users.noreply.github.com>
2024-05-07 10:29:05 +0530

    Delete experiment 5 directory

--- experiment 5/exp.js
+++
@@ -1,8 +0,0 @@
-var http = require("http");
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "content-Type": "text/html" });
-    res.write("hello world!");
-    res.end();
-  })
-  .listen(8080);
